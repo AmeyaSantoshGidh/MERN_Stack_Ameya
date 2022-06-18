2b0937bc0344b08a45e31659daa6ae6dec18a8f1
AmeyaSantoshGidh <98058342+AmeyaSantoshGidh@users.noreply.github.com>
2022-06-18 12:28:36 -0400

    Add files via upload

--- AmeyaSantoshGidh-backend/dao/reviewsDAO.js
+++ AmeyaSantoshGidh-backend/dao/reviewsDAO.js
@@ -1,9 +1,21 @@
 // import app from './server.js';
-import mongodb from 'mongodb';
+import mongodb, { ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
+import { throws } from 'assert';
 // import MoviesDAO from './moviesDAO.js';
 
+// Node js has access to file system (from server side).
+
+const MongoClient = import("mongodb").MongoClient;
+const dbUrl = "mongodb://0.0.0.0:27017";
+const dbName = " movie_time_db";   
+const dbCollection = "reviews";
+const PORT = 5000;
+let db,colection;
 let reviews;
+let c = 0;
+
+
 export default class ReviewsDAO{
     static async injectDB(conn){
         if(reviews){
@@ -14,6 +26,8 @@ export default class ReviewsDAO{
             console.log(`Unable to establish connection handle in reviewsDA: ${e}`);
         }
     }
+ 
+    
 
 static async addReview(movieId,user,review,date){
 try{
@@ -25,37 +39,74 @@ try{
         movie_id: ObjectId(movieId)
     }
     return await reviews.insertOne(reviewDoc);
-}catch(e){
-    console.error(`Unable to post review: ${e}`);
-    return {error:e};
+    }catch(e){
+        console.error(`Unable to post review: ${e}`);
+        return {error:e};
+    }
 }
+static async updateReview(reviewId,userId, review,date){
+ 
+    MongoClient.connect(dbUrl,function(err,client){
+        if(err){
+            console.log("Failed to make a connection",err);
+        }
+    
+        else{
+            console.log("Connected successfully to db server");
+            db = client.db(dbName);
+            collection = db.collection(dbCollection);
+            let editItem = collection.find({reviewId:reviewId}).toArray(function(err,docs){
+                if(err){
+                    response.writeHead(500);
+                    response.end(err);
+                }
+                else{
+                    response.writeHead(200,{"Content-type":"application/JSON"});
+                    response.end(JSON.stringify(docs))
+                    c = 1;
+                }
+    
+            });
+        if(c === 1){    
+        let query_val = editItem.updateOne({"review": review }, {$set:{"review":review,"date":date}});
+            if(query_val.modifiedCount <= 0){
+                throw new error();
+            }
+    }              
+        }
+    });
+    
+    
 }
-static async updateReview(reviewId,userId, review,date){}
 
 static async deleteReview(reviewId,userId){
+    MongoClient.connect(dbUrl,function(err,client){
+        if(err){
+            console.log("Failed to make a connection",err);
+        }
+    
+        else{
+            console.log("Connected successfully to db server");
+            db = client.db(dbName);
+            collection = db.collection(dbCollection);
+            let editItem = collection.find({reviewId:reviewId}).toArray(function(err,docs){
+                if(err){
+                    response.writeHead(500);
+                    response.end(err);
+                }
+                else{
+                    response.writeHead(200,{"Content-type":"application/JSON"});
+                    response.end(JSON.stringify(docs))
+                    c = 1;
+                }
+    
+            });
+        if(c === 1){    
 
+        let query_val = editItem.deleteOne({"userId":userId});
+    }              
+   }
+});
+    
 }
 }
-// async function main() {
-//     dotenv.config();
-
-//     const client = new mongodb.MongoClient(
-//         process.env.MOVIEREVIEWS_DB_URI
-//     )
-//     const port = process.env.PORT || 8000;
-
-//     try {
-//         // Connect to MongoDB server
-//         await client.connect();
-//         await MoviesDAO.injectDB(client);
-
-//         app.listen(port, () => {
-//             console.log("server is running on port :" + port);
-//         })
-//     } catch (e) {
-//         console.error(e);
-//         process.exit(1);
-//     }
-// }
-
-// main().catch(console.error);
